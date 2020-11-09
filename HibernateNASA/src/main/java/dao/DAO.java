25b5cf65953bc62f42444cd1a44ccbefaed4dad8
Reis-Silva J.C <cesar.everlastingtechnologydeveloper@outlook.com>
2020-11-09 16:22:59 -0300

    Ajustes Gerais

--- HibernateNASA/src/main/java/dao/DAO.java
+++ HibernateNASA/src/main/java/dao/DAO.java
@@ -1,5 +1,14 @@
 package dao;
 
-public interface DAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+
+public interface DAO<T,I> {
+	
+	 public void save(T entity);
+	 public void remove(Class<T> classGeneric, I pk);
+	 T getById(Class<T> classGeneric, I pk);
+	 public List<T> buscar();
+	 EntityManager getEntityManager();
 }
